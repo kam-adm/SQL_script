2c6e7634f02cea649512777ea4f1fc9dae83a33a
Troll <kam-adm@mail.ru>
2023-05-09 19:27:30 +0300

    changes

--- Script_SQL.sql
+++ Script_SQL.sql
@@ -1,13 +1,13 @@
 
 CREATE TABLE IF NOT EXISTS Music_genres (
     ID_genres SERIAL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL
+	name VARCHAR(30) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Artists(
     ID_Artists SERIAL PRIMARY KEY,
 	ID_genres INTEGER REFERENCES Music_genres(ID_genres),
-	name VARCHAR(30) NOT null
+	name VARCHAR(30) NOT NULL
 );
 	
 CREATE TABLE IF NOT EXISTS Albums (
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS Track (
     ID_Track SERIAL  PRIMARY KEY,
     ID_Albums INTEGER REFERENCES Albums(id_Album),
 	name VARCHAR(40) NOT NULL,
-	password VARCHAR(128) NOT null
+	duration INTEGER NOT NULL CHECK(duration < 20)
 );	
 
 
@@ -36,9 +36,14 @@ CREATE TABLE IF NOT EXISTS Albums_and_artists (
 );
 
 CREATE TABLE IF NOT EXISTS Collection (
-    name VARCHAR(40) NOT NULL,
-    year INTEGER NOT NULL,
-	Albums_ID INTEGER REFERENCES Albums(ID_Album),
+    ID_Collection SERIAL  PRIMARY KEY,
+    name VARCHAR(40) NOT NULL UNIQUE,
+    year INTEGER NOT NULL CHECK(year >= 1960 AND year <= 2023),
+	Track_ID INTEGER REFERENCES Track(ID_Track)
+);
+
+CREATE TABLE IF NOT EXISTS Track_collections (
 	Track_ID INTEGER REFERENCES Track(ID_Track),
-	CONSTRAINT pk3 PRIMARY KEY (Albums_ID, Track_ID)
+	Collection_ID INTEGER REFERENCES Collection(ID_Collection),
+	CONSTRAINT pk3 PRIMARY KEY (Track_ID, Collection_ID)
 );
